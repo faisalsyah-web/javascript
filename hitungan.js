778350b3211c46931c28a06a92797cb015815e89
faisalsyah-web <faisalsyahruddin57@gmail.com>
2022-12-27 21:46:22 +0900

    lanjut latihan Javascript

--- hitungan.js
+++ hitungan.js
@@ -1,14 +1,18 @@
-  let hitungan = 0;
+if (!localStorage.getItem('hitungan')){
+    localStorage.setItem('hitungan', 0);
+}
+
       function hitung() {
+        let hitungan = localStorage.getItem('hitungan');
         hitungan++;
         document.querySelector("h1").innerHTML = hitungan;
-
-        if (hitungan % 10 === 0) {
-          alert(`Hitungan sekarang ${hitungan}`);
-        }
+        localStorage.setItem('hitungan', hitungan)
       }
 
       document.addEventListener('DOMContentLoaded', function(){
+        document.querySelector("h1").innerHTML = localStorage.getItem('hitungan');
         document.querySelector("button").onclick = hitung;
+
+        // setInterval(hitung, 1000);
       });
       
